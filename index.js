f972cd665252fc4ed49750e8ffd996c87f66c0ca
Todd Greenfield <todd.greenfield@me.com>
2017-07-01 18:14:52 -0400

    Added reverse

--- index.js
+++ index.js
@@ -14,10 +14,13 @@ function DummySwitch(log, config) {
   this.log = log;
   this.name = config.name;
   this.stateful = config.stateful;
-
+  this.reverse = config.reverse;
+  
   this._service = new Service.Switch(this.name);
   this._service.getCharacteristic(Characteristic.On)
     .on('set', this._setOn.bind(this));
+
+  if (this.reverse) this._service.setCharacteristic(Characteristic.On, true);
 }
 
 DummySwitch.prototype.getServices = function() {
@@ -28,11 +31,14 @@ DummySwitch.prototype._setOn = function(on, callback) {
 
   this.log("Setting switch to " + on);
 
-  if (on && !this.stateful) {
+  if (on && !this.reverse && !this.stateful) {
     setTimeout(function() {
       this._service.setCharacteristic(Characteristic.On, false);
     }.bind(this), 1000);
+  } else if (!on && this.reverse && !this.stateful) {
+    setTimeout(function() {
+      this._service.setCharacteristic(Characteristic.On, true);
+    }.bind(this), 1000);
   }
-
   callback();
 }
