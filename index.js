a83296fa63f58f487a1be17f085e19fb4be67a4b
Todd Greenfield <todd.greenfield@me.com>
2017-10-24 07:08:43 -0400

    added persistence for stateful

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 "use strict";
 
-var Service, Characteristic;
+var Service, Characteristic, HomebridgeAPI;
 
 module.exports = function(homebridge) {
 
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-
+  HomebridgeAPI = homebridge;
   homebridge.registerAccessory("homebridge-dummy", "DummySwitch", DummySwitch);
 }
 
@@ -15,12 +15,25 @@ function DummySwitch(log, config) {
   this.name = config.name;
   this.stateful = config.stateful;
   this.reverse = config.reverse;
-  
   this._service = new Service.Switch(this.name);
+  
+  this.cacheDirectory = HomebridgeAPI.user.persistPath();
+  this.storage = require('node-persist');
+  this.storage.initSync({dir:this.cacheDirectory, forgiveParseErrors: true});
+  
   this._service.getCharacteristic(Characteristic.On)
     .on('set', this._setOn.bind(this));
 
   if (this.reverse) this._service.setCharacteristic(Characteristic.On, true);
+
+  if (this.stateful) {
+	var cachedState = this.storage.getItemSync(this.name);
+	if((cachedState === undefined) || (cachedState === false)) {
+		this._service.setCharacteristic(Characteristic.On, false);
+	} else {
+		this._service.setCharacteristic(Characteristic.On, true);
+	}
+  }
 }
 
 DummySwitch.prototype.getServices = function() {
@@ -40,5 +53,10 @@ DummySwitch.prototype._setOn = function(on, callback) {
       this._service.setCharacteristic(Characteristic.On, true);
     }.bind(this), 1000);
   }
+  
+  if (this.stateful) {
+	this.storage.setItemSync(this.name, on);
+  }
+  
   callback();
 }
