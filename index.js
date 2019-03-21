0778f6a990af841e2916e6962427bbc0902abd33
NorthernMan54 <buyminivan-northern@yahoo.ca>
2019-03-20 21:02:23 -0400

    Switched from switch to lightbulb to allow brightness to be controlled

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ function DummySwitch(log, config) {
   this.stateful = config.stateful;
   this.reverse = config.reverse;
   this.contact = config['contact'] || false;
-  this._service = new Service.Switch(this.name);
+  this._service = new Service.Lightbulb(this.name);
+  this._service
+    .addCharacteristic(Characteristic.Brightness);
 
   this._contact = new Service.ContactSensor(this.name);
 
