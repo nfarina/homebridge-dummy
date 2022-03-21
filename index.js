cb91cb1d281388792343c0ca3f2aa68ac18473ef
ptysell <9489667+ptysell@users.noreply.github.com>
2022-03-20 22:04:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   HomebridgeAPI = homebridge;
-  homebridge.registerAccessory("homebridge-dummy-Outlet", "DummyOutlet", DummyOutlet);
+  homebridge.registerAccessory("homebridge-dummy-accessory", "DummyAccessory", DummyAccessory);
 }
 
 
