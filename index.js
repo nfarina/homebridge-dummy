9b568a476f7e366b3a45548ee5b004ecffa2540e
ptysell <9489667+ptysell@users.noreply.github.com>
2022-03-20 22:29:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -26,9 +26,10 @@ function DummyAccessory(log, config) {
   this.timer = null;
 	
 switch (this.accessorytype) {
-	case 'Switch': this._service = new Service.Switch(this.name);
-		case 'Outlet': this._service = new Service.Outlet(this.name);
-		case 'Light': this._service = new Service.Lightbulb(this.name);
+	case 'Switch': this._service = new Service.Switch(this.name);break;
+		case 'Outlet': this._service = new Service.Outlet(this.name);break;
+		case 'Light': this._service = new Service.Lightbulb(this.name);break;
+		
 
 		
 }
